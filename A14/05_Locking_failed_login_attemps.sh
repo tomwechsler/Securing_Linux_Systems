71f20dd671653dd3af5ba9f1406cb53a3228cca8
Tom Wechsler <tom.wechsler@outlook.com>
2024-04-27 16:03:11 +0200

    lines edited

--- A14/05_Locking_failed_login_attemps.sh
+++ A14/05_Locking_failed_login_attemps.sh
@@ -8,22 +8,29 @@ su - ubuntu
 
 exit
 
+#Install the package
+sudo apt install libpam-modules
+
 #Check the man pages
-man pam_tally2
+man pam_faillock.conf
 
 #Edit the file
 sudo vim /etc/pam.d/common-auth
 
-#Too many comments
-sudo sed -i -E '/^(#|$)/d' /etc/pam.d/common-auth
+#The First line
+auth    required pam_faillock.so preauth audit silent deny=3 unlock_time=300
 
-#Edit the file
-sudo vim /etc/pam.d/common-auth
-
-auth required pam_tally2.so deny=3 unlock_time=300
-#Edit auth requisite/required   pam_deny.so
+#This lines bevor auth requisite pam_deny.so
+auth    [default=die] pam_faillock.so authfail audit deny=3 unlock_time=300
+auth    sufficient pam_faillock.so authsucc audit deny=3 unlock_time=300
 
 #Save and exit
+
+#Edit the file
+sudo vim /etc/pam.d/common-account
+
+#At the end
+account    required pam_faillock.so
 
 #Loggon with ubuntu more than 3 times (wrong password)
 su - ubuntu
