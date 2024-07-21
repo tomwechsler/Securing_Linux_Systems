53ae4d90b77c7557039697a20310496fdddccbb9
Tom Wechsler <tom.wechsler@outlook.com>
2024-07-21 15:46:23 +0200

    lines edited

--- A12/02_Creating_certificate_signing_request.sh
+++ A12/02_Creating_certificate_signing_request.sh
@@ -14,8 +14,13 @@ file /tmp/ubuntu.csr
 #Set a password for vagrant
 passwd vagrant
 
+#Search for the PasswordAuthentication
+sudo grep -R 'PasswordAuthentication' /etc/ssh/
+
 #We set the PasswordAuthentication to yes - to be able to use scp
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 
 #Restart the service
 systemctl restart sshd
